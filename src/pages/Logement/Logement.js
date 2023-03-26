7e7eed4f5178c5c1bb88029a0fe239e63b3ab947
Aklyss <romuald-samson@hotmail.com>
2023-03-26 22:03:36 +0200

    1

--- src/pages/Logement/Logement.js
+++ src/pages/Logement/Logement.js
@@ -5,12 +5,12 @@ import { stockData } from "../../datas/datas"
 import { useState } from 'react'
 import Carrousel from '../../components/Carrousel/Carrousel'
 import Collapse from '../../components/Collapse/Collapse'
-
+import note1 from '../../Images/Logos/icone1.png'
+import note2 from '../../Images/Logos/icone2.png'
 
 export default function Logement() {
     const params = useParams()
     const data = stockData.find(x => x.id === params.id)
-    const [equ, setEqu] = useState(false);
 
     return (
         <div className='main'>
@@ -31,7 +31,12 @@ export default function Logement() {
                             <p>{data.host.name}</p>
                             <img src={data.host.picture}></img>
                         </span>
-                        <p>{data.rating}</p>
+                        <div>
+                            {[...Array(5)].map((star, index) => {
+                            return (
+                                <img className='etoile' key={index} src={index + 1 <= data.rating  ? note1 : note2 } alt="star" />
+                            )
+                        })}</div>
                     </div>
                 </div>
                 <div className='info'>
